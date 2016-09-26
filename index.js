38a1c93b3210cd33d3a27e55c255afdd4ca65cba
Jospeh Kalb <kalb.joseph@gmail.com>
2016-09-26 09:49:06 -0700

    second commit

--- index.js
+++ index.js
@@ -11,35 +11,23 @@ var planets = [
   ['Mercury', 0.377],
   ['Sun', 27.9]
 ];
-var gravity = planets.slice(0);
-
 /* Append option into #names, add the value, add the text[0]
 just to keep the first part of the text array*/
+
 // making the drop down list
 planets.reverse();
-console.log('First position in gravity is: ' + gravity[0][0]);
-console.log("First position is planets is: " + planets[0][0]);
 $.each(planets, function(val, text) { 
   $('#names').append( $('<option>').val(text[1]).html(text[0])); 
 });
 
 // swapping gravity positions for sorting and then creating table
-$.each(gravity, function(val, text){
-  var grav = text[1];
-  var name = text[0];
-  gravity[val][0] = grav;
-  gravity[val][1] = name;
-});
-gravity.sort();
-console.log('New first position in gravity is: ' + gravity[0][0]);
-console.log("New first position is planets is: " + planets[0][0]);
-// create the planet sorting table
-$.each(gravity, function(val, text){
-  $('table').append($('<tr><td>' + text[1] + 
-    '</td><td>' + text[0] + '</td></tr>')); // appends the planet name and gravity
+planets.sort(function(a, b) { return a[1] - b[1] }); // sorts everything the planets by gravity
+$.each(planets, function(val, text){
+  $('table').append($('<tr><td>' + text[0] + 
+    '</td><td>' + text[1] + '</td></tr>')); // appends the planet name and gravity
 });
 
-// the button will make the magic happen
+// the button will still make the magic happen
 $("button").click(function() {
   var weight = $("input").val(); // grab user input from html
   var planet = $("select option:selected").val(); // grabs select option value (position)
